4236063a376dfe2830127d7ac1bfb8ab0abb62c7
Prajakta Desai <111192248+prajakta1011@users.noreply.github.com>
2026-07-02 11:08:01 +0530

    Updated file

--- 10functions2.js
+++ 10functions2.js
@@ -1,1 +1,15 @@
-function 
+let isIncrementdone = () => {
+    let salary = 400000;
+    
+    let increment = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            salary += 20000;
+            resolve(salary);
+        }, 2000);
+    });
+    return increment;
+}
+
+isIncrementdone().then((salary) => {
+    console.log("Updated salary:", salary);
+});
